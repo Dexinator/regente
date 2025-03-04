9813ed18ae23229f9b87f670e890b81d47e0056e
Dexinator <badillo.delgado.jorge@gmail.com>
2025-03-04 08:27:08 -0600

    perimera version de ordenes

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,js,jsx,ts,tsx}"], // ✅ Asegurar que Astro detecte Tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
