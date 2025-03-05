572cf63f5f29ac87e37fc48bbfc45bf094f16f11
Dexinator <badillo.delgado.jorge@gmail.com>
2025-03-05 14:01:27 -0600

    more and more

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,8 +1,9 @@
-import pool from "../../lib/db";
+import { connectDB } from "../../lib/db";
 
 export async function GET() {
   try {
-    const [rows] = await pool.query("SELECT 1 + 1 AS resultado");
+    const db = await connectDB(); // Usa la conexión a través del túnel SSH
+    const [rows] = await db.execute("SELECT 1 + 1 AS resultado");
     return new Response(JSON.stringify({ success: true, resultado: rows[0].resultado }), {
       headers: { "Content-Type": "application/json" },
     });
